f4625de84df3f30f18f77b0b16046412f8750ba6
Pladot <matheuscapellari22@gmail.com>
2024-09-24 14:47:18 -0300

    Criei a conexao com o banco de dados

---
+++ src/main/java/com/example/jdbctest/Conexao.java
@@ -0,0 +1,25 @@
+package com.example.jdbctest;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+public class Conexao {
+    private String banco = "lojinha";
+    private String usuario = "root";
+    private String senha = "";
+
+    public Connection getConexao(){
+
+        String conecta = "jdbc:mysql://localhost:3306/" + banco;
+        Connection conexao = null;
+
+        try {
+            conexao = DriverManager.getConnection(conecta, usuario, senha);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return conexao;
+    }
+}
