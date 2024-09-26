647aaf5b36b9f8dad715be6dd238b8f21b827313
Pladot <matheuscapellari22@gmail.com>
2024-09-26 15:42:18 -0300

    add o cadastrar no CategoriaController

---
+++ src/main/java/com/example/jdbctest/controller/CategoriaController.java
@@ -0,0 +1,19 @@
+package com.example.jdbctest.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLException;
+
+import com.example.jdbctest.model.Categoria;
+
+
+@RestController
+public class CategoriaController {
+    @PostMapping("categoria/cadastrar")
+    public Categoria cadastrar(Categoria categoria) throws SQLException{
+        categoria.insert();
+        
+        return categoria;
+    }
+}
