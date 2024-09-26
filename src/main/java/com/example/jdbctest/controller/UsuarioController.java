2a011a30b1adbac272df0e0e46c189602a4d88ae
Pladot <matheuscapellari22@gmail.com>
2024-09-26 14:51:15 -0300

    add o update no UsuarioController

--- src/main/java/com/example/jdbctest/controller/UsuarioController.java
+++ src/main/java/com/example/jdbctest/controller/UsuarioController.java
@@ -7,15 +7,25 @@ import com.example.jdbctest.model.Usuario;
 import java.sql.SQLException;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 @RestController
 public class UsuarioController {
-    @PostMapping("/usuario")
+    @PostMapping("/usuario/cadastrar")
     public Usuario cadastrar(Usuario usuario) throws SQLException {
         usuario.insert();
         
         return usuario;
     }
+
+    @PostMapping("usuario/atualizar")
+    public Usuario atualizar(Usuario usuario) throws SQLException{
+        usuario.update();        
+
+        return usuario;
+    }    
     
 }
